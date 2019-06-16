df4d6102492cbfed055393c3690b6718d29900ed
David Zhang <cajan2@163.com>
2019-06-16 15:20:34 +0800

    consumer.php; add Dockerfile for springmvcprovider

--- hybrid/consumer/consumer.php
+++ hybrid/consumer/consumer.php
@@ -1,8 +1,14 @@
 <?php
 
 $name = getenv("TARGET");
+if (empty($name)) {
+    $name = "localhost:9080";
+}
 $url = "http://" . $name . "/hello";
-
+//echo $name;
+//echo "<br />";
+//echo $url;
+//echo "<br />";
 $client = curl_init($url);
 curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($client);
