b3dd4757a7e6a8a23f15ffbb35c3ce57f4ed8195
David Zhang <cajan2@163.com>
2019-06-16 16:48:02 +0800

    change deployment file for kubernates

--- hybrid/springmvcprovider/start.sh
+++ hybrid/springmvcprovider/start.sh
@@ -3,4 +3,4 @@
 # CMDVAR="-Djava.security.egd=file:/dev/./urandom","java -agentlib:jdwp=transport=dt_socket,address=0:8000,server=y,suspend=n -jar"
 java $CMDVAR \
 -Dcse.service.registry.address="${REGISTRY:-"http://127.0.0.1:30100"}" \
--jar ./jar_target/springmvcprovider-0.0.1-SNAPSHOT.jar
+-jar ./jar_target/springmvcprovider-0.0.1-SNAPSHOT-exec.jar
